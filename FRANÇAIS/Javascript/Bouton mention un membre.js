b7ef3af306e41f4a16a28cca7f97335b480dbf95
Kimberley Bonix <bonixkimberley@gmail.com>
2021-11-09 13:51:38 +0100

    Easier mention correction

--- FRANÇAIS/Javascript/Bouton mention un membre.js
+++ FRANÇAIS/Javascript/Bouton mention un membre.js
@@ -7,40 +7,46 @@
 // https://help.forumotion.com/t140358-make-the-mentioning-system-easier-to-use
 
 
+/* FORUM VERSIONS
+ * 0 = PHPBB2
+ * 1 = PHPBB3
+ * 2 = PUNBB
+ * 3 = INVISION
+ * 4 = MODERNBB
+ */
 $(function() {
-   var version = 0;
+  var version = 4;
  
-   if (/mode=reply/.test(window.location.search) && my_getcookie('fa_mention')) {
-      document.post.message.value += '@"' + my_getcookie('fa_mention') + '" ';
-      my_setcookie('fa_mention',''); 
-   } if (!/\/t\d+/.test(window.location.pathname)) return;
+  if (/mode=reply/.test(window.location.search) && my_getcookie('fa_mention')) {
+    document.post.message.value += '@"' + my_getcookie('fa_mention') + '" ';
+    my_setcookie('fa_mention','');
+  } if (!/\/t\d+/.test(window.location.pathname)) return;
  
-   for (var a = $(['ul.profile-icons'][version]), b, i = 0, j = a.length, t = document.getElementById('text_editor_textarea'); i<j; i++) {
-      b = document.createElement('A');
-     
-      b.title = 'Mentionner ' + $(a[i]).text(); // texte au survol de la souris
-      b.style.marginRight = '3px';
-      b.className = 'fa-mention'; // .class du lien
-      b.innerHTML = 'Taguer'; // texte dans le lien
-     
-      b.href = '#';
-      b.onclick = function() {
-         var n = this.title.replace(/^.*?\s/,'');
-    
-         if ($.sceditor) t.insertText('@"' + n + '" ');
-         else {
-            my_setcookie('fa_mention', n);
-            window.location.href = '/post?t=' + window.location.pathname.replace(/\/t(\d+)-.*/,'$1') + '&mode=reply';
-         }
-    
-         return false;
-      };
+  for (var a = $(['.post-options', '.profile-icons', '.post-options', '.posting-icons', '.profile-icons'][version]), b, c, d = ['.name strong a', '.author a', '.username a', '.author a', '.post_pseudo a'][version], e, i = 0, j = a.length, t = document.getElementById('text_editor_textarea'), l = version == 1 || version == 3 || version == 4; i<j; i++) {
+    b = document.createElement('A');
+    b.className = 'fa-mention';
+    b.innerHTML = '@';
+    b.title = 'Mentionner ' + $(a[i]).closest('.post_row').find(d + ':not(.fa-mention)').text();
+    b.onclick = function() {
+      var n = this.title.replace(/^.*?\s/,'');
  
-      a[i].parentNode.insertBefore(b, a[i]);
-   }
+      if ($.sceditor) t.insertText('@"' + n + '" ');
+      else {
+        my_setcookie('fa_mention', n);
+        window.location.href = '/post?t=' + window.location.pathname.replace(/\/t(\d+)-.*/,'$1') + '&mode=reply';
+      }
+    };
  
-   $(function(){
-      if (!$.sceditor) return;
-      t=$(t).sceditor('instance');
-   });
+    if (l) {
+      c = document.createElement('LI');
+      c.appendChild(b);
+    }
+ 
+    a[i].insertBefore(l ? c : b, a[i].firstChild);
+  }
+ 
+  $(function(){
+    if (!$.sceditor) return;
+    t=$(t).sceditor('instance');
+  });
 });
