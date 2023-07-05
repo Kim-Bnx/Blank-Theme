beb0a8b26829276733a656ccf3e78318d54b214b
Kimberley Bonix <bonixkimberley@gmail.com>
2023-07-05 02:05:36 +0200

    VERSION 3.2
    Nouveau script pour copier le contenu d'un bloc de code + personnalisation du bouton "copier"

--- FRANÇAIS/Javascript/Sélectionner contenu d'un code.js
+++ FRANÇAIS/Javascript/Sélectionner contenu d'un code.js
@@ -1,32 +1,54 @@
 // PLACEMENT : sur toutes les pages
 
-// [FRENCH] by Ea
-// https://forum.forumactif.com/t309759-bouton-selectionner-le-contenu-pour-les-balises-de-code
+// [ENGLISH] by Ange Tuteur
+// https://fmdesign.forumotion.com/t1337-copy-code
 
-// [ENGLISH] by Base
-// https://help.forumotion.com/t93456-select-content-button
+// Code alternatif pour simplement sélectionner le code
+// [FRENCH] by Ea : https://forum.forumactif.com/t309759-bouton-selectionner-le-contenu-pour-les-balises-de-code
 
-function selectCode(e) {
-    var s = $(e).closest("dl").find(".cont_code,code").get(0), range, selection;
-    var a = s, z = s;
-    while(a.nodeType == 1 && a.childNodes.length) a=a.firstChild;
-    while(z.nodeType == 1 && z.childNodes.length) z=z.lastChild;
-    if (!$(a).is('.fixff')) {
-        var fix = $('<span class="fixff"/>').insertBefore(a);
-    } else {
-      a = a.nextSibling;
+$.getScript('https://cdn.jsdelivr.net/clipboard.js/1.5.16/clipboard.min.js', function() {
+  window.fae_copyCode = {
+    copy : 'Copier',
+    copied : '<i class="bi bi-check2"></i> Copié'
+  };
+ 
+  $(function() {
+    $('code').closest('.codebox').attr('id','codeBox');
+    
+    var a = $('.codebox dt').not('.spoiler > dt, .hidecode > dt'),
+        i = 0,
+        j = a.length;
+ 
+    if (a[0]) {
+      $('head').append('<style type="text/css">.fae_copy-code{float:right;cursor:pointer;}.fae_copy-code:hover{text-decoration:underline}</style>');
+ 
+      for (; i < j; i++) {
+        a[i].insertAdjacentHTML('beforeend', '<div class="copyCode">' + fae_copyCode.copy + '</div>');
+      }
+ 
+      new Clipboard('.copyCode',{
+        target : function (copy) {
+          if (copy.innerHTML != fae_copyCode.copied) {
+            return $(copy).closest('.codebox').find('.cont_code, code')[0];
+          }
+        }
+ 
+      }).on('success', function (e) {
+        var copy = e.trigger;
+ 
+        if (copy.innerHTML != fae_copyCode.copied) {
+          copy.innerHTML = fae_copyCode.copied;
+          copy.className += ' copiedCode';
+ 
+          window.setTimeout(function() {
+            copy.innerHTML = fae_copyCode.copy;
+            copy.className = copy.className.replace('copiedCode', '');
+          }, 2000);
+        }
+ 
+        e.clearSelection();
+      });
     }
-    if (document.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(s);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.setStart(a, 0);
-        range.setEnd(z, z.nodeValue ? z.nodeValue.length : 0);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-};
-$(function(){$("dl.codebox:not(.spoiler,.hidecode)  > dd.code, dl.codebox:not(.spoiler,.hidecode)  > dd > code").closest("dl").find('dt').append('<span onClick="selectCode(this)" class="selectCode">Sélectionner</span>')});
+ 
+  });
+});
