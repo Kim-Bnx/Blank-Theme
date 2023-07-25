eb74e1c935e5a066e277c1786df43ec742be69f0
Kimberley Bonix <bonixkimberley@gmail.com>
2023-07-25 11:57:20 +0200

    Ajouts de commentaire explicatif

--- Javascript/Champs de profil.js
+++ Javascript/Champs de profil.js
@@ -6,62 +6,71 @@
 // Options, by Monomer
 // Possibilité de déplacer un champs dans un autre élément
 
-!function() {
+!function () {
 
-	const settings = {
-                semicolon: true, // false = retire les (:) après un nom de champs
-		cleanUp: true,
-          
-          // Option pour déplacer un champs dans un autre élément, laissez vide pour désactiver 
-          move: ['champ', 'champ'], // Nom du champs en MINISCULE et SANS ACCENT (exemple : 'message') et séparer les champs à deux mots par un tiret (exemple : 'etat-civil')
-          moveTo: '.class' // élément dans lequel les champs seront déplacés
-	},
+    const settings = {
+        semicolon: true, // false = retire les (:) après un nom de champs
+        cleanUp: true,
 
+        // GERER LE DEPLACEMENT DES CHAMPS DE PROFIL
+        //
+        // CIBLER UN CHAMP :
+        // - Nom du champ en MINISCULE et SANS ACCENT (exemple : 'message' ou 'date-dinscription') 
+        // - Remplacez les espaces par un tiret (exemple : 'etat-civil')
+        // - Listez plusieurs champs par une virgule (exemple : 'champ', 'champ')
+        //
+        // LE DEPLACER :
+        // La class de l'élément dans lequel les champs seront déplacés
 
-	slugify = str => {
-        const from = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;',
-        to = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------',
-        reg = new RegExp(from.split('').join('|'), 'g');
+        moveTab: ['champ', 'champ'], //-- cibler un champ
+        moveToTab: '.class', //-- le déplacer
 
-        return str.trim().toLowerCase()
-        		.replace(/\s+/g, '-')
-        		.replace(reg, c => to.charAt(from.indexOf(c)))
-        		.replace(/&/g, '-and-')
-        		.replace(/[^\w\-]+/g, '')
+    },
+
+        slugify = str => {
+            const from = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;',
+                to = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------',
+                reg = new RegExp(from.split('').join('|'), 'g');
+
+            return str.trim().toLowerCase()
+                .replace(/\s+/g, '-')
+                .replace(reg, c => to.charAt(from.indexOf(c)))
+                .replace(/&/g, '-and-')
+                .replace(/[^\w\-]+/g, '')
                 .replace(/\-\-+/g, '-')
                 .replace(/^-+/, '')
                 .replace(/-+$/, '');
-    },
+        },
 
-    hideSemicolon = (label, name) => {
-        if (label.firstElementChild)
-            label.lastChild.remove();
-        else
-            label.textContent = name;
-    },
+        hideSemicolon = (label, name) => {
+            if (label.firstElementChild)
+                label.lastChild.remove();
+            else
+                label.textContent = name;
+        },
 
-    main = _ => {
+        main = _ => {
 
-    	document.querySelectorAll('.user_field').forEach(p => {
-    		const labelcontainer = p.querySelector('.field_label'),
-    		label = labelcontainer.querySelector('.label'),
-            name = label.textContent.replace(/ *: *$/, ''),
-          slug = slugify(name);
+            document.querySelectorAll('.user_field').forEach(p => {
+                const labelcontainer = p.querySelector('.field_label'),
+                    label = labelcontainer.querySelector('.label'),
+                    name = label.textContent.replace(/ *: *$/, ''),
+                    slug = slugify(name);
 
-    		p.classList.add('field-' + slug);
-          	
-          if(settings.move.includes(slug)) {
-          p.closest('.post_profile').querySelector(settings.moveTo).appendChild(p);
-        }
+                p.classList.add('field-' + slug);
 
-    		if (settings.cleanUp) {
-    			labelcontainer.textContent = settings.semicolon ? name + ': ' : name;
-    		} else if (!settings.semicolon) {
-                hideSemicolon(label, name)
-    		}
-    	});
+                if (settings.move.includes(slug)) {
+                    p.closest('.post_row').querySelector(settings.moveTo).appendChild(p);
+                }
 
-    };
+                if (settings.cleanUp) {
+                    labelcontainer.textContent = settings.semicolon ? name + ': ' : name;
+                } else if (!settings.semicolon) {
+                    hideSemicolon(label, name)
+                }
+            });
+
+        };
 
     document.addEventListener('DOMContentLoaded', main);
 }();
