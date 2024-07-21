4aa687844f776984fccadcbbe8ba05901bb1cc90
Kimberley Bonix <bonixkimberley@gmail.com>
2024-07-22 00:26:42 +0200

    New user field script

--- Javascript/Champs de profil.js
+++ Javascript/Champs de profil.js
@@ -1,76 +1,78 @@
-// PLACEMENT : Sur les sujets
+/** * Script pour ForumActif * Attribue un identifiant unique aux champs de profil dans les messages * Optionnellement, permet de déplacer ces champs de profil
+ *
+ * Placement : sur les sujets
+ * Documentation : https://blankthemerpg.forumactif.com/t188-personnaliser-les-champs-de-profil-dans-les-messages
+ * Contributeurs : Flerex, Monomer, 'Christa Lostmindy
+ */
 
-// [SPANISH] by Flerex
-// https://flerex.dev/entradas/clases-unicas-a-los-campos-del-miniperfil
+!(function () {
+  const SETTINGS_PROFILE = {
+      semicolon: false, //false = retire les (:) après un nom de champ
+      selectorField: ".user_field", //Le sélecteur d'un champ de profil
+      selectorPost: ".post", //Le sélecteur d'un message entier
 
-// Options, by Monomer
-// Possibilité de déplacer un champs dans un autre élément
-
-!function () {
-
-    const settings = {
-        semicolon: true, // false = retire les (:) après un nom de champs
-        cleanUp: true,
-
-        // GERER LE DEPLACEMENT DES CHAMPS DE PROFIL
-        //
-        // CIBLER UN CHAMP :
-        // - Nom du champ en MINISCULE et SANS ACCENT (exemple : 'message' ou 'date-dinscription') 
-        // - Remplacez les espaces par un tiret (exemple : 'etat-civil')
-        // - Listez plusieurs champs par une virgule (exemple : 'champ', 'champ')
-        //
-        // LE DEPLACER :
-        // La class de l'élément dans lequel les champs seront déplacés
-
-        move: ['champ', 'champ'], //-- cibler un champ
-        moveTo: '.class', //-- le déplacer
-
+      // Optionnel : déplacer les éléments
+      moveFromTo: [
+        {
+          target: ".classeCible1",
+          fields: ["elementDeplace1", "elementDeplace2", "elementDeplace3"],
+        },
+        {
+          target: ".classeCible2",
+          fields: ["elementDeplace5", "elementDeplace6"],
+        },
+      ],
     },
+    /**
+     * Turns the given string into a slug
+     * Transforme la chaine de caractères donnée en identifiant
+     * @param {String} str The field text label
+     * @returns {String} The newly generated slug
+     */
+    slugify = (str) => {
+      const from = "àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;",
+        to = "aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------",
+        reg = new RegExp(from.split("").join("|"), "g");
 
-        slugify = str => {
-            const from = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;',
-                to = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------',
-                reg = new RegExp(from.split('').join('|'), 'g');
+      return str
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(reg, (c) => to.charAt(from.indexOf(c)))
+        .replace(/&/g, "-and-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+    },
+    /**
+     * Find if a given field's slug is among the list of fields that should be moved elsewhere, and returns the selector of the target
+     * Vérifie qu'un identifant attribué à un champ fait partie de la liste de champs à déplacer, et renvoie le sélecteur de la cible
+     * @param {String} slug The slug attributed to the field
+     * @return {String} The selector of the target if the tested field should be moved
+     */
+    findTarget = (slug) => {
+      let target;
+      SETTINGS_PROFILE.moveFromTo.forEach((i) => {
+        if (i.fields.includes(slug)) target = i.target;
+      });
+      return target;
+    },
+    main = (_) => {
+      document.querySelectorAll(SETTINGS_PROFILE.selectorField).forEach((champ) => {
+        const label = champ.querySelector(".label"),
+          name = label.textContent.replace(/ *: *$/, ""),
+          slug = slugify(name);
 
-            return str.trim().toLowerCase()
-                .replace(/\s+/g, '-')
-                .replace(reg, c => to.charAt(from.indexOf(c)))
-                .replace(/&/g, '-and-')
-                .replace(/[^\w\-]+/g, '')
-                .replace(/\-\-+/g, '-')
-                .replace(/^-+/, '')
-                .replace(/-+$/, '');
-        },
+        champ.classList.add("field-" + slug);
 
-        hideSemicolon = (label, name) => {
-            if (label.firstElementChild)
-                label.lastChild.remove();
-            else
-                label.textContent = name;
-        },
+        let movingSomewhere = findTarget(slug);
+        if (movingSomewhere) {
+          champ.closest(SETTINGS_PROFILE.selectorPost).querySelector(movingSomewhere)?.appendChild(champ);
+        }
 
-        main = _ => {
-
-            document.querySelectorAll('.user_field').forEach(p => {
-                const labelcontainer = p.querySelector('.field_label'),
-                    label = labelcontainer.querySelector('.label'),
-                    name = label.textContent.replace(/ *: *$/, ''),
-                    slug = slugify(name);
-
-                p.classList.add('field-' + slug);
-
-                if (settings.move.includes(slug)) {
-                    p.closest('.post_row').querySelector(settings.moveTo).appendChild(p);
-                }
-
-                if (settings.cleanUp) {
-                    labelcontainer.textContent = settings.semicolon ? name + ': ' : name;
-                } else if (!settings.semicolon) {
-                    hideSemicolon(label, name)
-                }
-            });
-
-        };
-
-    document.addEventListener('DOMContentLoaded', main);
-}();
+        label.textContent = SETTINGS_PROFILE.semicolon ? name + ": " : name;
+      });
+    };
+  document.addEventListener("DOMContentLoaded", main);
+})();
